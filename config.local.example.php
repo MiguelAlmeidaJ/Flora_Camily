e408d2e084587528fea406a80434ec33d22329b3
Miguel Almeida <100483395+MiguelAlmeidaJ@users.noreply.github.com>
2026-09-17 14:54:38 -0300

    chore: adiciona exemplo de configuração local

---
+++ config.local.example.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'whatsapp_number' => '5532999999999',
+    'db' => [
+        'host' => 'localhost',
+        'name' => 'NOME_DO_BANCO',
+        'user' => 'USUARIO_DO_BANCO',
+        'pass' => 'SENHA_DO_BANCO',
+    ],
+];
